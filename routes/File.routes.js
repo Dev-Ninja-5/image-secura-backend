22807be9f91625ab31f89727e19b3d5272ee82e8
Dev-Ninja-5 <133943503+Dev-Ninja-5@users.noreply.github.com>
2023-12-31 01:07:41 +0530

    removed somepart of code

--- routes/File.routes.js
+++ routes/File.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {FileModel}=require("../models/File.model")
-const { noteData } = require("../controllers/Note.controller");
+
 const multer = require("multer");
 
 const fileRouter = express.Router();
